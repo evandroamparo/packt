8b1c1ce5a07de1c7dedab7d5e217a591e6068462
Evandro da Silva Amparo <1426462+evandroamparo@users.noreply.github.com>
2023-04-13 00:21:15 +0000

    handle blank description and add rating

--- book.py
+++ book.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.packtpub.com/free-learning'
-response = requests.get(url)
+URL = 'https://www.packtpub.com/free-learning'
+response = requests.get(URL, timeout=1000)
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
@@ -10,15 +10,20 @@ soup = BeautifulSoup(html, 'html.parser')
 book = soup.find('div', class_='grid product-info main-product')
 
 title = book.find('h3', class_='product-info__title').text
+stars = book.find('div', class_='product-info__rating').find('span').text
 author = book.find('span', class_='product-info__author free_learning__author').text
 author = author.split("By")
-author = "".join(author).strip()
+author = "".join(author).replace('\n', '').strip()
 publication_date = book.find('div', class_='free_learning__product_pages_date').find('span').text
 pages = book.find('div', class_='free_learning__product_pages').find('span').text
-description = book.find('div', class_='free_learning__product_description').find('span').text
+description = book.find('div', class_='free_learning__product_description').find('span')
+
+if description:
+    description = description.text
 image_url = book.find('img', class_='product-image')['src']
 
 print('Title:', title)
+print('Rating:', stars)
 print('Author:', author)
 print('Publication date:', publication_date)
 print('Pages:', pages)
